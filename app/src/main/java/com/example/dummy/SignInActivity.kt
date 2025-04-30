cd26845b48eed165397aa90b98516699f58783c1
Tazeema khan <tazimakhan40@gmail.com>
2025-04-30 15:24:13 -0700

    login screen

--- app/src/main/java/com/example/dummy/SignInActivity.kt
+++ app/src/main/java/com/example/dummy/SignInActivity.kt
@@ -2,22 +2,49 @@ package com.example.dummy
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
         val signUpText = findViewById<TextView>(R.id.tv_sign_up)
         val cancelButton = findViewById<TextView>(R.id.tv_cancel)
+        val loginButton = findViewById<Button>(R.id.btn_login)
+        val editTextUsername = findViewById<EditText>(R.id.et_username)
+        val editTextPassword = findViewById<EditText>(R.id.et_password)
 
-
+        firebaseAuth = FirebaseAuth.getInstance()
         signUpText.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
+        }
+
+        loginButton.setOnClickListener {
+            val email = editTextUsername.text.toString()
+            val password = editTextPassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
+            }
         }
 
         cancelButton.setOnClickListener {
